249520ced93d37875cb50d83d219f5398579fab8
Kieran Campbell <kieranrcampbell@gmail.com>
2015-09-10 14:44:42 +0100

    updating plotting; main plot and pseudotime traces working

--- plotting/plots.R
+++ plotting/plots.R
@@ -1,11 +1,36 @@
 
 # Generate plots for Bayesian Gaussian process latent variable models
+# kieran.campbell@sjc.ox.ac.uk
 
 library(ggplot2)
 library(rhdf5)
-#library(cowplot)
 library(dplyr)
 library(reshape2)
+library(magrittr)
+
+# Functions for covariance matrix calcualtion ---------
+
+cov_mat <- function(t, lambda, sigma) {
+  tt <- as.matrix(dist(t))^2
+  S <- exp(-lambda * tt) + sigma * diag(length(t))
+  S
+}
+
+cross_cov_mat <- function(t1, t2, lambda, sigma) {
+  n1 <- length(t1)
+  n2 <- length(t2)
+  tt <- matrix(0, nrow = n1, ncol = n2)
+  for(i in 1:n1) {
+    for(j in 1:n2) {
+      tt[i, j] <- (t1[i] - t2[j])^2
+    }
+  }
+  # tt <- tt + t(tt)
+  S <- exp(-lambda * tt)
+  S
+}
+
+# Analysis starts here --------------
 
 setwd("/net/isi-scratch/kieran/GP/gpseudotime/plotting/")
 
@@ -19,37 +44,75 @@ sigma <- h5read(h5file, "synthetic/sigma")
 n <- h5read(h5file, "synthetic/n")
 lambda <- h5read(h5file, "synthetic/lambda")
 
-pred <- h5read(h5file, "ppm/X")
+# Read posterior traces from file ----------
+tchain <- h5read(h5file, "mh/tchain")
+lchain <- h5read(h5file, "mh/lambda_chain")
+schain <- h5read(h5file, "mh/sigma_chain")
+ll <- h5read(h5file, "mh/log_lik")
+prior <- h5read(h5file, "mh/prior")
 
-df_synth <- data.frame(x = X[,1], y = X[,2], p1 = pred[,1], p2 = pred[,2], Pseudotime = t_gt)
+# Compute posterior mean estimates -----
+burn <- as.integer(nrow(tchain) / 2)
+tmap <- colMeans(tchain[burn:nrow(tchain),])
+lmap <- colMeans(lchain[burn:nrow(lchain),])
+smap <- colMeans(schain[burn:nrow(schain),])
 
-synth_plt <- ggplot(df_synth) + geom_point(aes(x=x, y=y, fill = Pseudotime), 
-                              shape = 21, size=3.5, colour = "gray20", alpha = .65) +
-  scale_fill_gradient(low = "gold", high = "darkred") + xlab("") + ylab("") +
-  geom_line(aes(x = p1, y = p2), linetype = 1, size = 1.2, colour = "black", alpha=.9) +
-  cowplot::theme_cowplot()
-synth_plt
 
+tp <- runif(200, min = 0, max = 1)
+mu <- matrix(NA, ncol = 2, nrow = length(tp))
+
+for(i in 1:2) {
+  K_map <- cov_mat(tmap, lmap[i], smap[i])
+  K_star_transpose <- cross_cov_mat(tp, tmap, lmap[i], smap[i])
+  matrix_prefactor <- K_star_transpose %*% solve(K_map)
+  mu[,i] <- matrix_prefactor %*% X[,i]
+}
+
+# Main plot -------
+
+df_x <- data.frame(cbind(X, tmap))
+names(df_x) <- c("x1", "x2", "tmap")
+df_mu <- data.frame(cbind(mu, tp))
+names(df_mu) <- c("mu1", "mu2", "t")
+df_mu <- arrange(df_mu, t)
+
+embedded_plt <- ggplot() + geom_point(data = df_x, aes(x = x1, y = x2, fill = tmap), color = "gray20", 
+                                      alpha = 0.65, size = 3.5, shape = 21) +
+  geom_path(data = df_mu, aes(x = mu1, y = mu2, colour = t), size = 3, alpha = 0.7) +
+  scale_fill_continuous_tableau() + scale_color_continuous_tableau()
+embedded_plt + ggtitle("Posterior mean curve")
 
 # Pseudotime Traces ------------------------------------------------------------------
 
-tchain = h5read(h5file, "mh/tchain")
-
 ## start by thinning the tchain a little
 set.seed(123)
-ttchain = tchain[c(TRUE, rep(FALSE, 9)),]
-dfttchain <- data.frame(ttchain)
-names(dfttchain) <- paste0('t', 1:ncol(dfttchain))
+dftchain <- data.frame(tchain)
+names(dftchain) <- paste0('t', 1:ncol(dftchain))
 
-to_sample <- sample(ncol(dfttchain), 30, replace = FALSE)
-dfttchain <- dfttchain[,to_sample]
-dfttchain$Iteration <- 10*(1:nrow(dfttchain))
+to_sample_bb <- paste0('t', sample(ncol(dftchain), 30, replace = FALSE))
+to_sample_trace <- paste0('t', sample(ncol(dftchain), 10, replace = FALSE))
 
-dfm <- melt(dfttchain, id.vars = "Iteration", variable.name = "Cell", value.name = "Pseudotime")
+dftchain$Iteration <- 10*(1:nrow(dftchain))
 
-pst_trace_plt <- ggplot(dfm) + geom_line(aes(x=Iteration, y=Pseudotime, colour = Cell)) +
+dfm <- melt(dftchain, id.vars = "Iteration", variable.name = "Cell", value.name = "Pseudotime")
+
+# data frame for converged trace (after burn in)
+df_converge <- dfm %>% 
+  filter(Cell %in% to_sample_trace) %>%
+  filter(Iteration > (as.integer(max(Iteration) / 2)))
+
+
+df_bigbang <- dfm %>%
+  filter(Cell %in% to_sample_bb) %>%
+  filter(Iteration < as.integer(max(Iteration) / 50))
+
+pst_trace_plt <- ggplot(df_converge) + geom_line(aes(x=Iteration, y=Pseudotime, colour = Cell)) +
   ggtitle("MCMC traces") + guides(colour = FALSE) + cowplot::theme_cowplot()
 pst_trace_plt
+
+bb_plt <- ggplot(df_bigbang) + geom_line(aes(x=Iteration, y=Pseudotime, colour = Cell)) +
+  ggtitle("MCMC traces") + guides(colour = FALSE) + cowplot::theme_cowplot()
+bb_plt
 
 
 # Kernel parameter traces -------------------------------------------------
